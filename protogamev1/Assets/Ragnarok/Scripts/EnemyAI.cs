fc01399b2a7dd3b32aefa3a0026b467f9a32c007
Michael Rosenberg <rosenbmr@sunyit.edu>
2015-03-20 00:56:14 -0400

    Return To Post Fixed!
    Optimized Enemy AI code even more than last time. Also fixed multiple
    bugs within AI, including AI gravity & collision issues, and a bug where
    enemy would not display walk animation when returning to "post" area.

--- protogamev1/Assets/Ragnarok/Scripts/EnemyAI.cs
+++ protogamev1/Assets/Ragnarok/Scripts/EnemyAI.cs
@@ -2,82 +2,167 @@
 using System.Collections;
 
 public class EnemyAI : MonoBehaviour {
-
-	private float maxSqrDistance = 4; //4 meters 
-	public float moveSpeed = 1f; //So you can tweak move speed in the editor or in enemy prefabs
-	public float gravity = -25f;
-	public float runSpeed = 8f;
-	public float groundDamping = 20f; // how fast do we change direction? higher means faster
-	public float inAirDamping = 5f;
-	public float jumpHeight = 3f;
+	// movement config
 	public GameObject target;
+	public double health = 100.00;
 
 	[HideInInspector]
+	private static GameObject WalkedOverObject;
 	private float normalizedHorizontalSpeed = 0;
+	private float gravity = -120f;
+	private float runSpeed = 1f;
+	private float groundDamping = 20f; // how fast do we change direction? higher means faster
+	private float inAirDamping = 5f;
+	private float jumpHeight = .75f;
 	private CharacterController2D _controller;
-	private Animator _animator;
+	private Animator animatorz;
 	private RaycastHit2D _lastControllerColliderHit;
 	private Vector3 _velocity;
-	private bool facingRight = true;
+	private float maxSqrDistance = 4;
 	private bool isDead = false;
-	private double Health = 100.00;
+	private bool isJumping = false;
+	private bool isRunning = false;
+	private bool isIdle = true;
+	private bool isHurt = false;
+	private bool isAttacking = false;
+	private bool isFacingRight = true;
+	private Vector3 velocity;
+	private Vector3 Origin;
+	float originReturnTimer = 4.0f;
 
 	void Awake()
 	{
-		_animator = GetComponent<Animator> ();
-		_controller = GetComponent<CharacterController2D> ();
+		animatorz = GetComponent<Animator> ();
+		_controller = GetComponent<CharacterController2D>();
+		// listen to some events for illustration purposes
 		_controller.onControllerCollidedEvent += onControllerCollider;
+		_controller.onTriggerEnterEvent += onTriggerEnterEvent;
+		_controller.onTriggerExitEvent += onTriggerExitEvent;
+		Origin = transform.position;
 	}
 
-	void onControllerCollider( RaycastHit2D hit )
-	{
-		// bail out on plain old ground hits cause they arent very interesting
-		if( hit.normal.y == 1f )
-			return;
-	}
+	#region Custom Functions
 
 	void Flip()
 	{
-		// Switch the way the player is labelled as facing
-		facingRight = !facingRight;
-		// Multiply the player's x local scale by -1
+		isFacingRight = !isFacingRight;
 		Vector3 theScale = transform.localScale;
 		theScale.x *= -1;
 		transform.localScale = theScale;
 	}
 	
-	void Update () {
-		
-		//Vector between the 2 entities
-		Vector3 dir = target.transform.position - transform.position;
-		
-		float distance = dir.sqrMagnitude, //Length squared because a sqrt is expensive
-		dot = Vector3.Dot(transform.forward, dir.normalized);
-		
-		if (distance < maxSqrDistance && distance > .1) 
-		{       //Is the distance less than what we want?
-			Debug.Log (distance);
-			if (dir.normalized.x >= 1) 
-			{ //Walk Right
-				if (facingRight == false)
-					Flip();
-				if (_controller.isGrounded)
-					_animator.Play (Animator.StringToHash ("Walk"));
-			} 
-			else if (dir.normalized.x <= 1) 
-			{ //Walk Left
-				if (facingRight == true)
-					Flip();
+	void TakeDamage(double dmgamt)
+	{
+		health -= dmgamt;
+		isHurt = true;
+	}
 
-				if (_controller.isGrounded)
-					_animator.Play (Animator.StringToHash ("Walk"));
+	void returnToOrigin(Vector3 pointOfOrigin)
+	{
+		
+		if (originReturnTimer > 0) 
+		{
+			originReturnTimer -= Time.deltaTime;
+		} 
+		else if (originReturnTimer <= 0) 
+		{
+			Vector3 dir = pointOfOrigin - transform.position;
+			if (dir.normalized.x > 0) 
+			{ 
+				if (isFacingRight == false)
+					Flip();
 			}
-			//Move in the direction, so towards the player
-			transform.position += dir.normalized * moveSpeed * Time.deltaTime;
-		} else {
-
-			if (_controller.isGrounded)
-				_animator.Play (Animator.StringToHash ("Idle"));
+			else if (dir.normalized.x < 0)
+			{
+				if (isFacingRight == true)
+					Flip();
+			}
+			transform.Translate(dir.normalized.x * runSpeed * Time.deltaTime, 0, 0);
 		}
 	}
+
+	#endregion
+
+	#region Event Listeners
+	
+	void onControllerCollider( RaycastHit2D hit )
+	{
+		// bail out on plain old ground hits cause they arent very interesting
+		if( hit.normal.y == 1f )
+			return;
+		
+		// logs any collider hits if uncommented. it gets noisy so it is commented out for the demo
+		//Debug.Log( "flags: " + _controller.collisionState + ", hit.normal: " + hit.normal );
+	}
+	
+	//WALK INTO EVENTS
+	void onTriggerEnterEvent( Collider2D col )
+	{
+
+	}
+	
+	
+	void onTriggerExitEvent( Collider2D col )
+	{
+		//Debug.Log( "onTriggerExitEvent: " + col.gameObject.name );
+	}
+	
+	#endregion
+	
+	
+	// the Update loop contains a very simple example of moving the character around and controlling the animation
+	void Update()
+	{
+		// grab our current _velocity to use as a base for all calculations
+		_velocity = _controller.velocity;
+		Vector3 dir = target.transform.position - transform.position;
+		float distance = dir.sqrMagnitude, //Length squared because a sqrt is expensive
+		dot = Vector3.Dot(transform.forward, dir.normalized);
+
+		if( _controller.isGrounded )
+			_velocity.y = 0;
+		
+		//var smoothedMovementFactor = _controller.isGrounded ? groundDamping : inAirDamping; // how fast do we change direction?
+		if (distance < maxSqrDistance && distance > .4) //.1
+		{
+			Debug.Log(dir.normalized.x);
+			int anim = 0;
+			if (dir.normalized.x > 0) 
+			{ 
+				//Run Right
+				if (isFacingRight == false)
+					Flip();
+			}
+			else if (dir.normalized.x < 0)
+			{
+				//Run Left
+				if (isFacingRight == true)
+					Flip();
+			}
+			animatorz.Play (Animator.StringToHash ("Walk"));
+			transform.Translate(dir.normalized.x * runSpeed * Time.deltaTime, 0, 0);
+			if (originReturnTimer != 4.0f)
+			{
+				originReturnTimer = 4.0f;
+			}
+		} 
+		else 
+		{
+			animatorz.Play (Animator.StringToHash ("Idle"));
+			if (transform.position != Origin)
+			{animatorz.Play (Animator.StringToHash ("Walk"));returnToOrigin (Origin);}
+			else {animatorz.Play (Animator.StringToHash ("Idle"));}
+		}
+		
+		// apply horizontal speed smoothing it
+		//var smoothedMovementFactor = _controller.isGrounded ? groundDamping : inAirDamping; // how fast do we change direction?
+		var smoothedMovementFactor = _controller.isGrounded ? groundDamping : inAirDamping; // how fast do we change direction?
+		_velocity.x = Mathf.Lerp( _velocity.x, normalizedHorizontalSpeed * runSpeed, Time.deltaTime * smoothedMovementFactor );
+		
+		// apply gravity before moving
+		_velocity.y += gravity * Time.deltaTime;
+		_controller.move( _velocity * Time.deltaTime );
+	}
+	
 }
+
--- protogamev1/Assets/Ragnarok/Scripts/RedEnemyAI.cs
+++ protogamev1/Assets/Ragnarok/Scripts/RedEnemyAI.cs
@@ -2,29 +2,47 @@
 using System.Collections;
 
 public class RedEnemyAI : MonoBehaviour {
+	// movement config
 	public GameObject target;
 	public double health = 100.00;
-
+	
 	[HideInInspector]
-	private float gravity = -25f;
+	private static GameObject WalkedOverObject;
+	private float normalizedHorizontalSpeed = 0;
+	private float gravity = -120f;
 	private float runSpeed = 1f;
 	private float groundDamping = 20f; // how fast do we change direction? higher means faster
 	private float inAirDamping = 5f;
-	private float jumpHeight = 3f;
+	private float jumpHeight = .75f;
+	private CharacterController2D _controller;
+	private Animator animatorz;
+	private RaycastHit2D _lastControllerColliderHit;
+	private Vector3 _velocity;
 	private float maxSqrDistance = 4;
-	private float normalizedHorizontalSpeed = 0;
 	private bool isDead = false;
-	private bool isAttacking = false;
 	private bool isJumping = false;
 	private bool isRunning = false;
 	private bool isIdle = true;
 	private bool isHurt = false;
+	private bool isAttacking = false;
 	private bool isFacingRight = true;
-	private Animator animatorz;
-	private RaycastHit2D _lastControllerColliderHit;
 	private Vector3 velocity;
-	private CharacterController2D _controller;
-
+	private Vector3 Origin;
+	float originReturnTimer = 3.5f;
+	
+	void Awake()
+	{
+		animatorz = GetComponent<Animator> ();
+		_controller = GetComponent<CharacterController2D>();
+		// listen to some events for illustration purposes
+		_controller.onControllerCollidedEvent += onControllerCollider;
+		_controller.onTriggerEnterEvent += onTriggerEnterEvent;
+		_controller.onTriggerExitEvent += onTriggerExitEvent;
+		Origin = transform.position;
+	}
+	
+	#region Custom Functions
+	
 	void Flip()
 	{
 		isFacingRight = !isFacingRight;
@@ -32,65 +50,119 @@ public class RedEnemyAI : MonoBehaviour {
 		theScale.x *= -1;
 		transform.localScale = theScale;
 	}
-
+	
 	void TakeDamage(double dmgamt)
 	{
 		health -= dmgamt;
 		isHurt = true;
 	}
-
-	// Use this for initialization
-	void Awake()
+	
+	void returnToOrigin(Vector3 pointOfOrigin)
 	{
-		animatorz = GetComponent<Animator> ();
-		_controller = GetComponent<CharacterController2D> ();
-		_controller.onControllerCollidedEvent += onControllerCollider;
+		//animatorz.StopPlayback();
+		if (originReturnTimer > 0) 
+		{
+			animatorz.Play (Animator.StringToHash ("Idle"));
+			originReturnTimer -= Time.deltaTime;
+		} 
+		else if (originReturnTimer <= 0) 
+		{
+			Vector3 dir = pointOfOrigin - transform.position;
+			MoveTo (dir);
+			//animatorz.Play (Animator.StringToHash ("Idle"));
+		}
 	}
+
+	void MoveTo(Vector3 moveTo)
+	{
+		if (moveTo.normalized.x > 0) 
+		{ 
+			//Run Right
+			if (isFacingRight == false)
+				Flip();
+		}
+		else if (moveTo.normalized.x < 0)
+		{
+			//Run Left
+			if (isFacingRight == true)
+				Flip();
+		}
+		animatorz.Play (Animator.StringToHash ("Walk"));
+		transform.Translate(moveTo.normalized.x * runSpeed * Time.deltaTime, 0, 0);
+	}
+	
+	#endregion
+	
+	#region Event Listeners
+	
 	void onControllerCollider( RaycastHit2D hit )
 	{
 		// bail out on plain old ground hits cause they arent very interesting
 		if( hit.normal.y == 1f )
 			return;
+		
+		// logs any collider hits if uncommented. it gets noisy so it is commented out for the demo
+		//Debug.Log( "flags: " + _controller.collisionState + ", hit.normal: " + hit.normal );
 	}
 	
-	// Update is called once per frame
-	void Update () 
+	//WALK INTO EVENTS
+	void onTriggerEnterEvent( Collider2D col )
 	{
+		
+	}
+	
+	
+	void onTriggerExitEvent( Collider2D col )
+	{
+		//Debug.Log( "onTriggerExitEvent: " + col.gameObject.name );
+	}
+	
+	#endregion
+	
+	
+	// the Update loop contains a very simple example of moving the character around and controlling the animation
+	void Update()
+	{
+		// grab our current _velocity to use as a base for all calculations
+		_velocity = _controller.velocity;
 		Vector3 dir = target.transform.position - transform.position;
 		float distance = dir.sqrMagnitude, //Length squared because a sqrt is expensive
 		dot = Vector3.Dot(transform.forward, dir.normalized);
-		velocity = _controller.velocity;
+		
 		if( _controller.isGrounded )
-			velocity.y = 0;
-		var smoothedMovementFactor = _controller.isGrounded ? groundDamping : inAirDamping; // how fast do we change direction?
-		//velocity.x = Mathf.Lerp( velocity.x, normalizedHorizontalSpeed * runSpeed, Time.deltaTime * smoothedMovementFactor );
+			_velocity.y = 0;
+		
+		//var smoothedMovementFactor = _controller.isGrounded ? groundDamping : inAirDamping; // how fast do we change direction?
 		if (distance < maxSqrDistance && distance > .4) //.1
 		{
-			Debug.Log(dir.normalized.x);
-			int anim = 0;
-			if (dir.normalized.x > 0) 
-			{ 
-				//Run Right
-				if (isFacingRight == false)
-					Flip();
-			}
-			else if (dir.normalized.x < 0)
+			MoveTo (dir);
+			if (originReturnTimer != 3.5f)
 			{
-				//Run Left
-				if (isFacingRight == true)
-					Flip();
+				originReturnTimer = 3.5f;
 			}
-			velocity.x = Mathf.Lerp( velocity.x, normalizedHorizontalSpeed * runSpeed, Time.deltaTime * smoothedMovementFactor );
-			animatorz.Play (Animator.StringToHash ("Walk"));
-			transform.Translate(dir.normalized.x * runSpeed * Time.deltaTime, 0, 0);
 		} 
 		else 
 		{
-			animatorz.Play (Animator.StringToHash ("Idle"));
+			if (transform.position.x <= Origin.x-.02 || transform.position.x >= Origin.x+.02)
+			{
+
+				returnToOrigin (Origin);
+			}
+			else 
+			{
+				animatorz.Play (Animator.StringToHash ("Idle"));
+			}
 		}
-
-
-
-
+		
+		// apply horizontal speed smoothing it
+		//var smoothedMovementFactor = _controller.isGrounded ? groundDamping : inAirDamping; // how fast do we change direction?
+		var smoothedMovementFactor = _controller.isGrounded ? groundDamping : inAirDamping; // how fast do we change direction?
+		_velocity.x = Mathf.Lerp( _velocity.x, normalizedHorizontalSpeed * runSpeed, Time.deltaTime * smoothedMovementFactor );
+		
+		// apply gravity before moving
+		_velocity.y += gravity * Time.deltaTime;
+		_controller.move( _velocity * Time.deltaTime );
 	}
+	
 }
+
